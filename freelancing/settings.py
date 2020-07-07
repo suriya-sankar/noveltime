7b164caab053801bf8d72d45ad4a86a9a9133e3c
suriya <suriyasg88@gmail.com>
2020-07-08 02:08:54 +0530

    d

--- freelancing/settings.py
+++ freelancing/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'assets/')
+    os.path.join(BASE_DIR,'freelancing/assets/')
 
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static-root")
